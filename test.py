97964593c4a4d5f83c271f3df4d3e8ac62cd318b
20221707 <163367270+20221707@users.noreply.github.com>
2024-04-20 15:23:29 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+# NLP_2024
+
+# 🌸💓 **Python  Language**
+
+## **Learning Python** using **Colab** ([Visit Colab homepage](https://colab.research.google.com/?utm_source=scs-index)) and **Github** ([Visit Colab homepage](https://github.com/))
+
+### **1. Sigh up github** ([Visit a tutorial on Youtube](https://www.youtube.com/watch?v=c-NikCpec7U))
+### **2. Sigh up Google Colaboratory** (aka. Colab) ([Visit a tutorial on Youtube](https://www.youtube.com/watch?v=2X_EU18OeYM))
+
+### **3. Useful Referencese**
+- Exercises 200 파이썬200제 by 장삼용. 정보문화사 2017
+- [Jump to Python](https://wikidocs.net/book/1)
+- [wikidocs for nltk](https://wikidocs.net/21667)
+
+### **4. Markdown Language**
+* [Emoji](https://gist.github.com/rxaviers/7360908)
+* [Markdown syntax](https://www.markdownguide.org/basic-syntax/)
+
+### **5. Class Materials**
+| # | Sessions | Keywords |
+|:--:|:--:|:--:|
+| 1 | Markdown Language 💑  | [syntax](https://github.com/ms624atyale/NLP_2024/blob/main/0_MarkDown4README_md.ipynb)|
